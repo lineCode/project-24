13049a6cc9976ec6f5c601a2a16adb5c1c629028
Simon Norman <zhymon.norman@gmail.com>
2022-07-20 20:32:00 -0400

    build: re-add basic player pawn and camera

---
+++ Source/TwentyFour_Caliber/TwentyFourGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TwentyFourGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TWENTYFOUR_CALIBER_API ATwentyFourGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
